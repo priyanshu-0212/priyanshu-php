965be43b478d19b6136e47e43e0f2be179fd06d0
Priyanshu Srivastava <118589098+priyanshu-0212@users.noreply.github.com>
2024-11-19 10:32:45 +0530

    Add files via upload

---
+++ guess.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<title>02d1421b</title> 
+<h1>Welcome to my guessing game</h1>
+
+<?php
+$correctnumber = 13; 
+if (isset($_GET['guess'])) {
+    // Get the guess from the URL
+    $guess = $_GET['guess'];
+
+    if (!is_numeric($guess)) {
+        echo "Your guess is not a number.";
+    } else if ($guess < $correctnumber) {
+        echo "Your guess is too low.";
+    } else if ($guess > $correctnumber) {
+        echo "Your guess is too high.";
+    } else {
+        echo "Congratulations - You are right!";
+    }
+} else {
+    
+    echo "Missing guess parameter. <br> Your guess is too short.";
+}
+?>
+</body>
+</html>
